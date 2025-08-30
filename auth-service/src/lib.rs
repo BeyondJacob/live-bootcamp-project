c3ae30dfe102007eef91a0d7d6313e0302720574
Jacob Owens <owensinnovation@gmail.com>
2025-08-30 12:14:24 -0500

    [Sprint 2][Task 2] Implement signup route error cases
    [Sprint 2][Task 2] Implement signup route error cases

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,12 @@
 use axum::{
-    routing::post, routing::Router, serve::Serve,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::post,
+    serve::Serve,
+    Json, Router,
 };
+use domain::AuthAPIError;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 use tower_http::services::ServeDir;
 
@@ -18,6 +24,27 @@ pub mod app_state;
 pub mod domain;
 pub mod routes;
 pub mod services;
+
+#[derive(Serialize, Deserialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+
+impl IntoResponse for AuthAPIError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
+            AuthAPIError::InvalidCredentials => (StatusCode::BAD_REQUEST, "Invalid credentials"),
+            AuthAPIError::UnexpectedError => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected error")
+            }
+        };
+        let body = Json(ErrorResponse {
+            error: error_message.to_string(),
+        });
+        (status, body).into_response()
+    }
+}
 
 impl Application {
     pub async fn build(app_state: AppState, address: &str) -> Result<Self, Box<dyn Error>> {
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,4 +1,4 @@
-use auth_service::routes::SignupResponse;
+use auth_service::{routes::SignupResponse, ErrorResponse};
 use crate::helpers::{get_random_email, TestApp};
 use serde_json::json;
 
@@ -85,4 +85,70 @@ async fn should_return_422_if_malformed_input() {
             test_case
         );
     }
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+        // The signup route should return a 400 HTTP status code if an invalid input is sent.
+        // The input is considered invalid if:
+        // - The email is empty or does not contain '@'
+        // - The password is less than 8 characters
+
+        // Create an array of invalid inputs. Then, iterate through the array and 
+        // make HTTP calls to the signup route. Assert a 400 HTTP status code is returned.
+        let app = TestApp::new().await;
+
+        let invalid_inputs = [
+            json!({
+                "email": "",
+                "password": "password123",
+                "requires2FA": false
+            }),
+            json!({
+                "email": "rust@example.com",
+                "password": "1234567",
+                "requires2FA": false
+            }),
+        ];
+
+    for input in invalid_inputs.iter() {
+        let response = app.post_signup(input).await;
+        assert_eq!(response.status().as_u16(), 400, "Failed for input: {:?}", input);
+
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to ErrorResponse")
+                .error,
+            "Invalid credentials".to_owned()
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    // Call the signup route twice. The second request should fail with a 409 HTTP status code    
+    let app = TestApp::new().await;
+
+    let body = json!({
+        "email": "rust@example.com",
+        "password": "password123",
+        "requires2FA": false
+    });
+
+    let response = app.post_signup(&body).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    let response = app.post_signup(&body).await;
+    assert_eq!(response.status().as_u16(), 409);
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "User already exists".to_owned()
+    );
 }
