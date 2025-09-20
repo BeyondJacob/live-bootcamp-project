6058d7adf745ba101d1017d71dc3a1a1cd65f5c9
Jacob Owens <owensinnovation@gmail.com>
2025-09-20 10:42:01 -0500

    [Sprint 5][Task 1] Set up Postgress
    [Sprint 5][Task 1] Set up Postgress

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -7,6 +7,7 @@ use axum::{
 };
 use domain::AuthAPIError;
 use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::error::Error;
 use tower_http::{cors::CorsLayer, services::ServeDir};
 
@@ -92,4 +93,9 @@ impl Application {
         println!("listening on {}", self.address);
         self.server.await
     }
+}
+
+pub async fn get_postgres_pool(url: &str) -> Result<PgPool, sqlx::Error> {
+    // Create a new PostgreSQL connection pool
+    PgPoolOptions::new().max_connections(5).connect(url).await
 }
--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,19 +1,24 @@
 use auth_service::{
     app_state::AppState, 
+    get_postgres_pool,
     services::{
         hashmap_user_store::HashmapUserStore, 
         hashset_banned_token_store::HashsetBannedTokenStore,
         hashmap_two_fa_code_store::HashmapTwoFACodeStore,
         mock_email_client::MockEmailClient,
     }, 
-    utils::constants::prod,
+    utils::constants::{prod, DATABASE_URL},
     Application,
 };
+use sqlx::PgPool;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() {
+    // We will use this PostgreSQL pool in the next task! 
+    let _pg_pool = configure_postgresql().await;
+
     // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
     // This is needed for Docker to work, which we will add later on.
     // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
@@ -29,6 +34,21 @@ async fn main() {
         .expect("Failed to build app");
 
     app.run().await.expect("Failed to run app");
+}
+
+async fn configure_postgresql() -> PgPool {
+    // Create a new database connection pool
+    let pg_pool = get_postgres_pool(&DATABASE_URL)
+        .await
+        .expect("Failed to create Postgres connection pool!");
+
+    // Run database migrations against our test database! 
+    sqlx::migrate!()
+        .run(&pg_pool)
+        .await
+        .expect("Failed to run migrations");
+
+    pg_pool
 }
 
 // async fn hello_handler() -> Html<&'static str> {
